d123e97e3ef268b5e5cc3771b8ac06395d552a75
Francislainy <francislainy.campos@gmail.com>
2023-03-21 04:30:13 +0000

    Refactor navbar for buttons using map

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,43 @@
-import React from 'react';
-import { AppBar, Toolbar, Button } from '@material-ui/core';
+import React, { useState } from 'react';
+import { AppBar, Button, Toolbar } from '@material-ui/core';
+
+const buttons = [
+    { label: 'Hello Talk', href: '/' },
+    { label: 'Moments', href: '/moments' },
+    { label: 'Connect', href: '/connect' },
+    { label: 'Learn', href: '/learn' },
+    { label: 'Me', href: '/me' },
+];
 
 function NavBar() {
+    const [activeButton, setActiveButton] = useState('');
+
+    const handleClick = (buttonName) => {
+        setActiveButton(buttonName);
+    };
+
     return (
         <AppBar position="static" style={{ backgroundColor: 'black' }}>
             <Toolbar>
-                <Button color="inherit" href="/">Hello Talk</Button>
-                <Button color="inherit" href="/moments">Moments</Button>
-                <Button color="inherit" href="/connect">Connect</Button>
-                <Button color="inherit" href="/learn">Learn</Button>
-                <Button color="inherit" href="/me">Me</Button>
+                {buttons.map((button) => (
+                    <Button
+                        key={button.label}
+                        // todo: fix styling to appear for selected button
+                        style={{
+                            backgroundColor: activeButton === button.label ? 'blue' : 'inherit',
+                            color: activeButton === button.label ? 'white' : 'inherit'
+                        }}
+                        onClick={() => handleClick(button.label)}
+                        href={button.href}
+                    >
+                        {button.label}
+                    </Button>
+                ))}
             </Toolbar>
         </AppBar>
     );
 }
 
 export default NavBar;
+
+
